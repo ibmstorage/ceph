b40d7decfea844ff3546fc4516da8b6c3e9a9117
Naman Munet <namanmunet@Namans-MacBook-Pro.local>
2024-06-25 17:24:51 +0530

    mgr/dashborad: RGW sync policy
    Fixes: https://tracker.ceph.com/issues/66576
    
    Resolves: rhbz#2298101
    
    Signed-off-by: Naman Munet <nmunet@redhat.com>
    (cherry picked from commit 94f9a2bf18f43cc5b05dd8c216fe9e0975969106)
    (cherry picked from commit 41014fce446790481ee4f38de68b2ab8d467fbfe)
    (cherry picked from commit 32e02625c02e553084e4f80f0a6169d98aebb071)

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-multisite-sync-policy/rgw-multisite-sync-policy.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-multisite-sync-policy/rgw-multisite-sync-policy.component.ts
@@ -1,61 +1,24 @@
 import { TitleCasePipe } from '@angular/common';
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-import { forkJoin as observableForkJoin, Observable, Subscriber } from 'rxjs';
-import { RgwDaemonService } from '~/app/shared/api/rgw-daemon.service';
+import { Component, OnInit } from '@angular/core';
 import { RgwMultisiteService } from '~/app/shared/api/rgw-multisite.service';
-import { ListWithDetails } from '~/app/shared/classes/list-with-details.class';
-import { CriticalConfirmationModalComponent } from '~/app/shared/components/critical-confirmation-modal/critical-confirmation-modal.component';
-import { ActionLabelsI18n, URLVerbs } from '~/app/shared/constants/app.constants';
-import { TableComponent } from '~/app/shared/datatable/table/table.component';
 import { CellTemplate } from '~/app/shared/enum/cell-template.enum';
-import { Icons } from '~/app/shared/enum/icons.enum';
-import { CdTableAction } from '~/app/shared/models/cd-table-action';
 import { CdTableColumn } from '~/app/shared/models/cd-table-column';
-import { CdTableFetchDataContext } from '~/app/shared/models/cd-table-fetch-data-context';
-import { CdTableSelection } from '~/app/shared/models/cd-table-selection';
-import { FinishedTask } from '~/app/shared/models/finished-task';
-import { Permission } from '~/app/shared/models/permissions';
-import { AuthStorageService } from '~/app/shared/services/auth-storage.service';
-import { ModalService } from '~/app/shared/services/modal.service';
-import { TaskWrapperService } from '~/app/shared/services/task-wrapper.service';
-import { URLBuilderService } from '~/app/shared/services/url-builder.service';
-
-const BASE_URL = 'rgw/multisite/sync-policy';
 
 @Component({
   selector: 'cd-rgw-multisite-sync-policy',
   templateUrl: './rgw-multisite-sync-policy.component.html',
-  styleUrls: ['./rgw-multisite-sync-policy.component.scss'],
-  providers: [{ provide: URLBuilderService, useValue: new URLBuilderService(BASE_URL) }]
+  styleUrls: ['./rgw-multisite-sync-policy.component.scss']
 })
-export class RgwMultisiteSyncPolicyComponent extends ListWithDetails implements OnInit {
-  @ViewChild(TableComponent, { static: true })
-  table: TableComponent;
-  @ViewChild('deleteTpl', { static: true })
-  deleteTpl: TemplateRef<any>;
-
+export class RgwMultisiteSyncPolicyComponent implements OnInit {
   columns: Array<CdTableColumn> = [];
   syncPolicyData: any = [];
-  tableActions: CdTableAction[];
-  selection = new CdTableSelection();
-  permission: Permission;
 
   constructor(
     private rgwMultisiteService: RgwMultisiteService,
-    private titleCasePipe: TitleCasePipe,
-    private actionLabels: ActionLabelsI18n,
-    private authStorageService: AuthStorageService,
-    private modalService: ModalService,
-    private taskWrapper: TaskWrapperService,
-    private router: Router,
-    private rgwDaemonService: RgwDaemonService
-  ) {
-    super();
-  }
+    private titleCasePipe: TitleCasePipe
+  ) {}
 
   ngOnInit(): void {
-    this.permission = this.authStorageService.getPermissions().rgw;
     this.columns = [
       {
         prop: 'uniqueId',
@@ -106,38 +69,6 @@ export class RgwMultisiteSyncPolicyComponent extends ListWithDetails implements 
         }
       }
     ];
-    this.rgwDaemonService.list().subscribe();
-    const getEditURL = () => {
-      if (this.selection.first().groupName && this.selection.first().bucket) {
-        return `${URLVerbs.EDIT}/${this.selection.first().groupName}/${
-          this.selection.first().bucket
-        }`;
-      }
-      return `${URLVerbs.EDIT}/${this.selection.first().groupName}`;
-    };
-    const addAction: CdTableAction = {
-      permission: 'create',
-      icon: Icons.add,
-      click: () => this.router.navigate([BASE_URL, { outlets: { modal: URLVerbs.CREATE } }]),
-      name: this.actionLabels.CREATE,
-      canBePrimary: (selection: CdTableSelection) => !selection.hasSelection
-    };
-    const editAction: CdTableAction = {
-      permission: 'update',
-      icon: Icons.edit,
-      click: () => this.router.navigate([BASE_URL, { outlets: { modal: getEditURL() } }]),
-      name: this.actionLabels.EDIT
-    };
-    const deleteAction: CdTableAction = {
-      permission: 'delete',
-      icon: Icons.destroy,
-      click: () => this.deleteAction(),
-      disable: () => !this.selection.hasSelection,
-      name: this.actionLabels.DELETE,
-      canBePrimary: (selection: CdTableSelection) => selection.hasMultiSelection
-    };
-    this.tableActions = [addAction, editAction, deleteAction];
-  }
 
   transformSyncPolicyData(allSyncPolicyData: any) {
     if (allSyncPolicyData && allSyncPolicyData.length > 0) {
@@ -211,8 +142,9 @@ export class RgwMultisiteSyncPolicyComponent extends ListWithDetails implements 
                 this.table.refreshBtn();
               }
             });
-        });
-      }
-    });
+          });
+          this.syncPolicyData = [...this.syncPolicyData];
+        }
+      });
   }
 }
